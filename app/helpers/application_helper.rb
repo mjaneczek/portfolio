c1e4e2fa4842b9b5723f30ee72870e8ad2be54c6
mjaneczek <michal.janeczek@ymail.com>
2013-07-20 01:21:21 +0200

    Dodanie bowera, boostrap, boostrap metro.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def full_title(title)
+    base_title = t("title")
+    title ? "#{title} | #{base_title}" : base_title
+  end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,9 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+Portfolio::Application.configure do
+  config.assets.append_path 'components'
+end
+
 # Initialize the Rails application.
 Portfolio::Application.initialize!
