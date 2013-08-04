b6970cb84a740b92b844c4d071382d848c53de70
mjaneczek <michal.janeczek@ymail.com>
2013-08-04 21:20:22 +0200

    Pierwsza "sztywna" wizja strony głównej.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def full_title(title)
     base_title = t("title")
-    title ? "#{title} | #{base_title}" : base_title
+    title.blank? ? base_title : "#{title} | #{base_title}"
   end
 end
