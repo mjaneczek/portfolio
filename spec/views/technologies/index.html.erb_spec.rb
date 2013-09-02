38605f8cfe843bd8241b662859983e8cb48199fe
mjaneczek <michal.janeczek@ymail.com>
2013-09-02 21:10:45 +0200

    Uwspólniony komponent listy elementów dla projektów i technologii.

--- spec/views/technologies/index.html.erb_spec.rb
+++ spec/views/technologies/index.html.erb_spec.rb
@@ -14,7 +14,7 @@ describe "technologies/index" do
 
   it "renders a list of technologies" do
     render
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => "Name".to_s, :count => 2
+
+    rendered.should have_text "Name", count: 2
   end
 end
