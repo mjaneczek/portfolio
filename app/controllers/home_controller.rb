30331370f8148d6d1f275fee48092b53aad46945
Michał Janeczek <michal.janeczek@ymail.com>
2013-09-11 22:26:51 +0200

    Poprawka helpera lewego menu.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
+  include HomeHelper
+  
 	def index
     Project.order("created_at DESC").take(5).each do |project|
       left_menu.register_item(project.name, project_path(project))
